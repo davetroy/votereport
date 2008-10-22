f01b4d5b367aac758ea4345d15ee570d8144d34c
David Troy <dave@popvox.com>
2008-10-22 17:39:56 -0400

    Tune report view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,6 @@ class ReportsController < ApplicationController
 
   # GET /reports
   def index
-    @reports = Report.find(:all)
+    @reports = Report.find(:all).paginate :page => @page, :total_entries => 200, :per_page => 20
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -64,4 +64,6 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+  
+  config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', :source => 'http://gems.github.com'
 end
