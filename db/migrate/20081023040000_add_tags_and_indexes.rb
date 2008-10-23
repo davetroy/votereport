54cff2216fadcdf453f5abc039f623b343c1ec16
David Troy <dave@popvox.com>
2008-10-23 05:24:03 -0400

    Tuned EPXX tag pattern

--- db/migrate/20081023040000_add_tags_and_indexes.rb
+++ db/migrate/20081023040000_add_tags_and_indexes.rb
@@ -1,14 +1,14 @@
 class AddTagsAndIndexes < ActiveRecord::Migration
   def self.up
-    [ { :pattern => 'machine',      :description => 'Problem with voting machine(s)', :score => 2 },
-      { :pattern => 'registration', :description => 'Problems with the registration process', :score => 3 },
-      { :pattern => 'wait:(\d+)',   :description => 'Indicates wait time', :score => 0 },
-      { :pattern => 'challenges',   :description => 'Widespread challenges occurring at the location', :score => 4 },
-      { :pattern => 'hava',         :description => 'Accessibility issues', :score => 2 },
-      { :pattern => 'ballots',      :description => 'Out or running out of paper ballots', :score => 3 },
-      { :pattern => 'good',         :description => 'Good overall experience', :score => 0 },
-      { :pattern => 'bad',          :description => 'Bad overall experience', :score => 3 },
-      { :pattern => 'EP[A-Z]{2}',   :description => 'Election Protection, with State', :score => 2 } ].each { |t| Tag.create(t) }
+    [ { :pattern => 'machine',           :description => 'Problem with voting machine(s)', :score => 2 },
+      { :pattern => 'registration',      :description => 'Problems with the registration process', :score => 3 },
+      { :pattern => 'wait:(\d+)',        :description => 'Indicates wait time', :score => 0 },
+      { :pattern => 'challenges',        :description => 'Widespread challenges occurring at the location', :score => 4 },
+      { :pattern => 'hava',              :description => 'Accessibility issues', :score => 2 },
+      { :pattern => 'ballots',           :description => 'Out or running out of paper ballots', :score => 3 },
+      { :pattern => 'good',              :description => 'Good overall experience', :score => 0 },
+      { :pattern => 'bad',               :description => 'Bad overall experience', :score => 3 },
+      { :pattern => '\sEP[A-Z]{2}\s?',   :description => 'Election Protection, with State', :score => 2 } ].each { |t| Tag.create(t) }
     
     add_column "reports", :wait_time, :integer
     
