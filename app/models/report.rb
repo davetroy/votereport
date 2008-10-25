848b52bdd2a4d475cb5cd3b9ee9a261f44b57416
David Troy <dave@popvox.com>
2008-10-25 12:53:27 -0400

    Fixed source hash for source_name

--- app/models/report.rb
+++ app/models/report.rb
@@ -38,10 +38,10 @@ class Report < ActiveRecord::Base
   end
   
   def source_name
-    { :SOURCE_TWITTER => 'Twitter',
-      :SOURCE_MOZES   => 'Mozes',
-      :SOURCE_IPHONE  => 'iPhone',
-      :SOURCE_VOICE   => 'Telephone' }[input_source_id]
+    { SOURCE_TWITTER => 'Twitter',
+      SOURCE_MOZES   => 'Mozes',
+      SOURCE_IPHONE  => 'iPhone',
+      SOURCE_VOICE   => 'Telephone' }[input_source_id]
   end
 
   private
