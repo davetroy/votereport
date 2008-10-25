49b5a40ff1c76c0ebccf8bf8deb8926c8d250a94
David Troy <dave@popvox.com>
2008-10-25 09:44:56 -0400

    Relocated locations to separate migration

---
+++ db/migrate/001_setup_locations.rb
@@ -0,0 +1,60 @@
+class SetupLocations < ActiveRecord::Migration
+  class Filter < ActiveRecord::Base; end
+
+  def self.up
+    create_table "location_aliases", :options=>'ENGINE=MyISAM', :force => true do |t|
+      t.column "text", :string, :limit => 80
+      t.column "location_id", :integer
+    end
+
+    add_index "location_aliases", ["text"], :name => "index_location_aliases_on_text", :unique => true
+    add_index "location_aliases", ["location_id"], :name => "index_location_aliases_on_location_id"
+
+    create_table "locations", :options=>'ENGINE=MyISAM', :force => true do |t|
+      t.column "address", :string
+      t.column "country_code", :string, :limit => 10
+      t.column "administrative_area", :string, :limit => 80
+      t.column "sub_administrative_area", :string, :limit => 80
+      t.column "locality", :string, :limit => 80
+      t.column "thoroughfare", :string, :limit => 80
+      t.column "postal_code", :string, :limit => 25
+      t.column "point", :point, :null => false
+      t.column "geo_source_id", :integer
+      t.column "filter_list", :string
+      t.timestamps
+    end
+
+    add_index "locations", ["point"], :name => "index_locations_on_point", :spatial=> true
+
+    create_table "filters", :options=>'ENGINE=MyISAM', :force => true do |t|
+      t.column "name", :string, :limit => 80
+      t.column "aliases", :string
+      t.column "title", :string, :limit => 80
+      t.column "center_location_id", :integer
+      t.column "radius", :integer
+      t.column "conditions", :text
+      t.column "zoom_level", :integer
+      t.column "state", :string, :limit => 2
+      t.column "country_code", :string, :limit => 2
+      t.timestamps
+    end
+
+    # Import filter set and reset centers; this is touchy, beware!
+    Filter.connection.execute(File.read(File.dirname(__FILE__) + '/../filters.sql'))
+    Filter.find(:all).each { |f| f.update_attribute(:center_location_id, Location.geocode(f.title).id) }
+
+    create_table "invalid_locations", :options=>'ENGINE=MyISAM', :force => true do |t|
+      t.column "text", :string, :limit => 80
+      t.column "unknown", :boolean
+    end
+
+    add_index "invalid_locations", ["text"], :name => "index_invalid_locations_on_text", :unique => true
+  end
+
+  def self.down
+    drop_table :location_aliases
+    drop_table :locations
+    drop_table :filters
+    drop_table :invalid_locations
+  end  
+end
--- db/migrate/20081019131013_create_reports.rb
+++ db/migrate/20081019131013_create_reports.rb
@@ -1,7 +1,5 @@
 class CreateReports < ActiveRecord::Migration
 
-  class Filter < ActiveRecord::Base; end
-
   def self.up
     create_table "reports" do |t|
       t.integer  "input_source_id"                    # 1 = twitter, 2 = sms, 3 = iPhone, 4 = asterisk
@@ -45,56 +43,6 @@ class CreateReports < ActiveRecord::Migration
       t.integer "score"
     end
     
-    
-    # Location-related tables
-    create_table "location_aliases", :options=>'ENGINE=MyISAM', :force => true do |t|
-      t.column "text", :string, :limit => 80
-      t.column "location_id", :integer
-    end
-
-    add_index "location_aliases", ["text"], :name => "index_location_aliases_on_text", :unique => true
-    add_index "location_aliases", ["location_id"], :name => "index_location_aliases_on_location_id"
-
-    create_table "locations", :options=>'ENGINE=MyISAM', :force => true do |t|
-      t.column "address", :string
-      t.column "country_code", :string, :limit => 10
-      t.column "administrative_area", :string, :limit => 80
-      t.column "sub_administrative_area", :string, :limit => 80
-      t.column "locality", :string, :limit => 80
-      t.column "thoroughfare", :string, :limit => 80
-      t.column "postal_code", :string, :limit => 25
-      t.column "point", :point, :null => false
-      t.column "geo_source_id", :integer
-      t.column "filter_list", :string
-      t.timestamps
-    end
-
-    add_index "locations", ["point"], :name => "index_locations_on_point", :spatial=> true
-
-    create_table "filters", :options=>'ENGINE=MyISAM', :force => true do |t|
-      t.column "name", :string, :limit => 80
-      t.column "aliases", :string
-      t.column "title", :string, :limit => 80
-      t.column "center_location_id", :integer
-      t.column "radius", :integer
-      t.column "conditions", :text
-      t.column "zoom_level", :integer
-      t.column "state", :string, :limit => 2
-      t.column "country_code", :string, :limit => 2
-      t.timestamps
-    end
-
-    # Import filter set and reset centers; this is touchy, beware!
-    Filter.connection.execute(File.read(File.dirname(__FILE__) + '/../filters.sql'))
-    Filter.find(:all).each { |f| f.update_attribute(:center_location_id, Location.geocode(f.title).id) }
-
-    create_table "invalid_locations", :options=>'ENGINE=MyISAM', :force => true do |t|
-      t.column "text", :string, :limit => 80
-      t.column "unknown", :boolean
-    end
-
-    add_index "invalid_locations", ["text"], :name => "index_invalid_locations_on_text", :unique => true
-
     create_table "report_filters", :options => 'ENGINE=MyISAM' do |t|
       t.column "report_id",   :integer
       t.column "filter_id",   :integer
@@ -110,10 +58,6 @@ class CreateReports < ActiveRecord::Migration
     drop_table :twitter_users
     drop_table :report_tags
     drop_table :tags
-    drop_table :location_aliases
-    drop_table :locations
-    drop_table :filters
     drop_table :report_filters
-    drop_table :invalid_locations
   end
 end
