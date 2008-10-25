a16ddc3b6b143be5eb548f25a6f625b4e6e1f35f
David Troy <dave@popvox.com>
2008-10-25 08:54:02 -0400

    Disable timestamp checking while in pre-production mode

--- lib/daemons/poll_mozes.rb
+++ lib/daemons/poll_mozes.rb
@@ -22,25 +22,20 @@ def debug(msg)
   puts "[poll_mozes] [debug] #{msg}" if RAILS_ENV == 'development'
 end
 
-def write_tstamp(time = Time.now)
-  File.open(STAMPFILE, "w") { |f| f.print time.to_s }
-end
-
 while($running) do
   since = Time.parse(File.read(STAMPFILE)) rescue 1.hour.ago
   begin
-    
     debug "Pulling XML feed..."
     doc = Hpricot.XML(open(FEED))
     
     (doc/:item).each do |item|
       begin        
         # only process items posted after our last check...
-        item_tstamp = Time.parse((item/:pubDate).inner_text)
-        if item_tstamp < since
-          debug "skipping item: #{item_tstamp} before #{since}"
-          next
-        end
+        # item_tstamp = Time.parse((item/:pubDate).inner_text)
+        # if item_tstamp < since
+        #   debug "skipping item: #{item_tstamp} before #{since}"
+        #   next
+        # end
                 
         # create the report
         debug "creating report..."
@@ -58,7 +53,7 @@ while($running) do
   rescue Exception => e
     puts "[poll_mozes] Uncaught exception during loop: \n#{e.class}: #{e.message}\n\t#{e.backtrace.join("\n")} "
   end
-  write_tstamp
+  File.open(STAMPFILE, "w") { |f| f.print time.to_s }
   sleep 10
 end
 
