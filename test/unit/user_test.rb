8419b08d7c3a3d9f4dc44e6e99adc547f3b6356b
Cory <cory.forsyth@gmail.com>
2008-10-29 23:13:34 -0400

    grouped user tests logically by test function

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -22,20 +22,28 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 'http://example.com/', user.url
     assert_equal 'foo@example.com', user.email
     assert !user.activated
+  end
   
+  def test_user_verification
+    user = User.create(@valid_attributes.merge(:url =>"http://api.com", :email =>"foo@api.com"))
+
     # User receives verification email and submits api_key for verification
     assert User.find_by_api_key(user.api_key).verify(user.api_key)
     assert user.reload
     assert user.verified
     assert user.activated
-    
+  
     # check default values
     %w(day_query_count day_update_count query_count update_count).each { |a| assert_zero user.send(a) }
     assert_equal DEFAULT_QUERY_LIMIT, user.day_query_limit
     assert_equal DEFAULT_UPDATE_LIMIT, user.day_update_limit
+  end
+  
+  def test_authentication
+    user = User.create(@valid_attributes.merge(:url => "http://authentication.com", :email => "foo@auth.com"))
     
     # check to be sure authentication works
-    assert_equal user.id, User.authenticate(:email => 'foo@example.com', :password => 'nodule')
+    assert_equal user.id, User.authenticate(:email => 'foo@auth.com', :password => 'nodule')
   end
   
   def test_valid_email
