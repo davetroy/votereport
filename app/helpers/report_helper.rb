9562fc008a36e7bbcd56cc4b081dae918bf34f98
David Troy <dave@popvox.com>
2008-10-30 17:03:55 -0400

    Tune audio links

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -16,7 +16,7 @@ module ReportHelper
   end
 
   def audio_link(report)
-    link_to("with audio", "#{report.reporter.audio_path}/#{report.audio_file}", :target => 'new') if report.has_audio
+    "<embed src='#{report.reporter.audio_path}/#{report.audio_file}' width='100' height='20' AUTOPLAY='false'/>" if report.has_audio
   end
 
   def bumpspark2( results )
