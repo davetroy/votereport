889ed0179d6e002a4171a424a331ac7d59758be7
David Troy <dave@popvox.com>
2008-11-04 17:52:20 -0500

    Re-enabled report_controller tests

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -7,18 +7,18 @@ class ReportsControllerTest < ActionController::TestCase
     assert !@response.body.include?("hdn_reports_container")
   end
   
-  # def test_live_autoupdate_index_loads
-  #   get(:index, :live => "1", :format => 'html')
-  #   assert_response :success
-  #   assert @response.body.include?("hdn_reports_container")
-  # end
+  def test_live_autoupdate_index_loads
+    get(:index, :live => "1", :format => 'html')
+    assert_response :success
+    assert @response.body.include?("hdn_reports_container")
+  end
   
-  # def test_reload_loads
-  #   get(:reload)
-  #   assert_response :success
-  #   exp_size = [50, Report.count].min
-  #   assert_equal exp_size, assigns(:reports).size
-  # end
+  def test_reload_loads
+    get(:reload)
+    assert_response :success
+    exp_size = [50, Report.count].min
+    assert_equal exp_size, assigns(:reports).size
+  end
   
   def test_reload_reads_count_param
     get(:reload, :per_page => 2)
