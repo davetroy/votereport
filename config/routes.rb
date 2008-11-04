f59144f40714147a50cf16fb334d010947c181ea
Andrew Turner <ajturner@highearthorbit.com>
2008-11-04 00:45:53 -0500

    Fixed reports.kml route

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,9 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect 'local/:filter', :controller => "reports", :action => "index"
 
-  map.kml_feed 'feeds/kml/:count.json', :controller => "reports", :action => "index"
-  map.json_feed 'feeds/json/:page.json', :controller => "reports", :action => "index"
-  map.json_state_feed 'feeds/json/:state/:page.json', :controller => "reports", :action => "index"
+  map.kml_feed 'feeds/:count.kml', :controller => "reports", :action => "index", :format => :kml
+  map.json_feed 'feeds/:page.json', :controller => "reports", :action => "index"
+  map.json_state_feed 'feeds/:state/:page.json', :controller => "reports", :action => "index"
 
   map.resources :reports, 
     :collection => {
